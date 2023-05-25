30032558b760a246d4d2bc3ef62af5ded2a4591d
daroczinimrod <daroczinimrod@gmail.com>
2023-05-25 11:49:12 +0200

    4. ora vege

--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Balkezesek;
+
+internal class orai
+{
+    static List<Class1> BalosLista = new List<Class1>();
+
+    static void Main(string[] args)
+    {
+        Beolvas();
+
+    }
+
+
+    private static void Beolvas()
+    {
+        StreamReader beolvas = new StreamReader("Balkezesek");
+        BalosLista.Skip(1);
+        while (!beolvas.EndOfStream) ;
+        {
+            string[] data = beolvas.ReadLine().Split(';');
+            Console.WriteLine(data);
+        }
+
+        beolvas.Close();
+    }
+}
